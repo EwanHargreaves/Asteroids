6f65a074f615c5eb61a68326676b8aa56f81b13e
EwanHargreaves <ewanhargreaves2002@gmail.com>
2024-08-19 21:07:52 +0100

    Limited game loop to 60 fps

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill((0,0,0))
         pygame.display.flip()
@@ -12,10 +15,8 @@ def main():
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  return
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
