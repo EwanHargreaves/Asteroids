239928e06515fdff2ed0c43add2b20f52f06fb20
EwanHargreaves <ewanhargreaves2002@gmail.com>
2024-08-19 21:26:26 +0100

    Drew player sprite in screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,8 +9,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         for event in pygame.event.get():
