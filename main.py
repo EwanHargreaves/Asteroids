e92bb5979753bd06f29380ebe2ce682bebc5797f
EwanHargreaves <ewanhargreaves2002@gmail.com>
2024-08-21 20:11:18 +0100

    Added player to groups as a futreproof

--- main.py
+++ main.py
@@ -9,21 +9,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, (drawable, updatable))
 
     while True:
         screen.fill((0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
 
-        player.update(dt)
-
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  return
         
         dt = clock.tick(60) / 1000
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -3,10 +3,11 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, groups):
         super().__init__(x,y,PLAYER_RADIUS)
         self.position = Vector2(x, y)
         self.rotation = 0
+        self.add(*groups)
 
     
     def triangle(self):
